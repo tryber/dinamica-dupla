f80824883cb7e094c01ba90ded3b501b1cd0d3f4
Bruce Alberto <brucealberto2@gmail.com>
2021-11-06 16:35:56 -0300

    Requisito 7 e 9

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const inputSenha = document.querySelector("#input-senha");
+const botaoForm = document.querySelector("#submit-btn");
 function checkCaracter() {
-  console.log(inputSenha.value);
+  const valor = inputSenha.value;
+  const regex = /\W|_/g;
+  if (
+    valor.match(/[A-Z]/g) &&
+    valor.match(/[a-z]/g) &&
+    valor.match(/[0-9]/g) &&
+    valor.match(regex)
+  ) {
+    inputSenha.style.borderBottom = "2px solid black";
+  } else {
+    inputSenha.style.borderBottom = "2px solid red";
+  }
 }
 
 inputSenha.addEventListener("keyup", checkCaracter);
