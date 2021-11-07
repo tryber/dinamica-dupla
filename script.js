3cc8ee7a61b939971bc2033895f404e8de823bc4
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2021-11-06 22:13:04 -0300

    requisitos 10 a 12

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const btn = document.querySelector('#submit-btn');
 const checkbox = document.querySelector('#agreement');
+const inputName = document.querySelector('#input-name');
+const inputEmail = document.querySelector('#input-email');
 
 checkbox.addEventListener('click', function(){
+  if (btn.disabled === true) {
+    btn.disabled = false;
+  } else {
+    btn.disabled = true;
+  }
+});
 
-  
+btn.addEventListener('click', function(event){
+  event.preventDefault();
+  const usuario = {
+    nome: inputName.value,
+    email: inputEmail.value
+  };
+  localStorage.setItem('user-infos', JSON.stringify(usuario));
 });
