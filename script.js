29547d0310e8ebcb626289aca95980895ebed16d
camila <camilaczeg@gmail.com>
2021-11-07 21:30:50 -0300

    Requisito 11

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let chekbox = document.querySelector('#agreement');
 let botao = document.querySelector('#submit-btn');
+const inputName = document.querySelector('#input-name');
+const inputEmail = document.querySelector('#input-email');
 
 chekbox.addEventListener('click', function(){
     if (chekbox.checked === false){
@@ -9,13 +11,17 @@ chekbox.addEventListener('click', function(){
     };
 })
 
+let info = {
+    usuario: {
+        nome: '',
+        email: '',
+    }
+}
 
-
-
-
-// function verificaChek() {
-//     if (chekbox.checked === false){
-//         botao.disabled = true
-//     }
-// }
-// verificaChek();
+botao.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    info.usuario.nome = inputName.value;
+    info.usuario.email = inputEmail.value;
+    localStorage.setItem('user-infos', JSON.stringify(info));
+})
